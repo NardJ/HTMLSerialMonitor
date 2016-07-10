44a8b7d8ca321b20510385c385d47e9de2e8bec3
Nard Janssens <nardjanssens@gmail.com>
2016-07-10 10:42:56 +0200

    Memory usage improvement
    Used char array instead of string to preserve memory.

--- Source/Arduino/HTMLSerialMonitor/examples/Bare_DecoratedText/Bare_DecoratedText.ino
+++ Source/Arduino/HTMLSerialMonitor/examples/Bare_DecoratedText/Bare_DecoratedText.ino
@@ -2,23 +2,25 @@
   Bare_DecoratedText.ino  - Sample sketch part of HTMLSerialMonitor package
                             No library used, demonstrates simple html text decoration 
   created:  26 May 2016
-  version:  27 May 2016
+  version:  10 Jul 2016
   author :  Nard Janssens
   license:  Attribution-NonCommercial-ShareAlike 2.5 Generic (CC BY-NC-SA 2.5)
 */
 
+const int analogPin=3;
 void setup(){   
     Serial.begin(9600);
 }
 
+char red[]="Value is: <b style='color:red;'>";
+char grn[]="Value is: <b style='color:green;'>";
+char eol[]="</b>";
+
 void loop(){
-  int valueD3=random(0,1024); // randomfrom 0 to 1024 
-  String red="Value is: <b style='color:red;'>";
-  String grn="Value is: <b style='color:green;'>";
-  String eol="</b>";
-  if (valueD3<512)  Serial.println(red+valueD3+eol);    
-  if (valueD3>=512) Serial.println(grn+valueD3+eol);    
+  int val = analogRead(analogPin);
+  if (val<256)  Serial.print(red);
+  if (val>=256) Serial.print(grn);
+  Serial.print(val);
+  Serial.println(eol);    
   delay (300);
 }
-
-
